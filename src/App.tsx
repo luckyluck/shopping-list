cfe6fdc9c75075b8065703ffc20c1edcabce8a62
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-03-04 18:21:02 +0100

    Add basic top navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, NavLink } from 'react-router-dom';
 
+import './App.css';
 import Login from './components/Login';
 import MealPlans from './components/MealPlans';
 import MealPlan from './components/MealPlan';
 
 function App() {
+  const token = sessionStorage.getItem('token');
+
   return (
     <Router>
+      <nav className={'Navigation'}>
+        <NavLink to={'/meal-plans'} activeClassName={'ActiveLink'}>Meal plans</NavLink>
+        {!token && <NavLink to={'/login'} activeClassName={'ActiveLink'}>Login</NavLink>}
+      </nav>
       <Switch>
         <Route path={['/meal-plans']} exact>
           <MealPlans/>
