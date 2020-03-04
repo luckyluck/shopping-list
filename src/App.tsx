f2798d21db0f1d00e06a7f905fd208135414fb11
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-03-04 12:31:31 +0100

    Add simple routing with basic login form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './components/Login';
 
 function App() {
   return (
-    <div>
-      <h1>Shopping list</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={'/'} exact>
+          <div>Main page</div>
+        </Route>
+        <Route path={'/login'} exact>
+          <Login/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Login/index.tsx
@@ -0,0 +1,58 @@
+import React, { FormEvent, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // If we already have a token, we should log user in automatically
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      history.push('/');
+    }
+  }, [history]);
+
+  const onSubmitHandler = (e: FormEvent): void => {
+    e.preventDefault();
+    setError('');
+    // Do not validate data intentionally
+    if (!username || !password) {
+      setError('Check your form fields');
+    } else {
+      fetch(
+        'https://ddapi.production.dietdoctor.com/auth/create',
+        { method: 'POST', body: JSON.stringify({ username, password }) }
+        )
+        .then(response => response.json())
+        .then(({ token }) => {
+          sessionStorage.setItem('token', token);
+          history.push('/');
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <form onSubmit={onSubmitHandler}>
+        <div>{error}</div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id={'email'} value={username} onChange={(e): void => setUsername(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id={'password'} value={password} onChange={(e): void => setPassword(e.target.value)}/>
+        </div>
+        <div>
+          <button type={'submit'}>Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
