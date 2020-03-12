78a51533084ee6044bf6b2017542f74d0e14b94d
alex-glomo <alexandr@glomo.se>
2020-03-12 16:37:14 +0100

    Replace react-scripts with custom webpack config. Update dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './public/index.html',
+});
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    app: './src/index.tsx'
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[hash].bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    /**
+     * All files inside webpack's output.path directory will be removed once, but the
+     * directory itself will not be. If using webpack 4+'s default configuration,
+     * everything under <PROJECT_DIR>/build/ will be removed.
+     * Use cleanOnceBeforeBuildPatterns to override this behavior.
+     *
+     * During rebuilds, all webpack assets that are not used anymore
+     * will be removed automatically.
+     *
+     * See more https://github.com/johnagan/clean-webpack-plugin
+     */
+    new CleanWebpackPlugin({
+      // Write Logs to Console
+      verbose: true
+    }),
+    htmlWebpackPlugin,
+    new Dotenv({
+      // Path to .env.development file (this is the default)
+      path: './.env',
+      // load .env.development.example (defaults to "false" which does not use dotenv-safe)
+      safe: false
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080,
+    historyApiFallback: true,
+    hot: true
+  }
+};
